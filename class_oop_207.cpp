8a1b0c613bd7368114d95a1d0a4a5a7a3781cf4e
shabit24 <hossain.shabit24@gmail.com>
2023-04-10 14:47:52 +0700

    Add project files.

---
+++ class_oop_207.cpp
@@ -0,0 +1,10 @@
+// class_oop_207.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
+

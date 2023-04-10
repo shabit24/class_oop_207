3c6119cf33a7f3e26c4f9a708b4e92019bd95d45
shabit24 <hossain.shabit24@gmail.com>
2023-04-10 15:01:31 +0700

    input function

--- class_oop_207.cpp
+++ class_oop_207.cpp
@@ -9,6 +9,21 @@ public:
 	string nama;
 	void tampile() {
 		cout << "NIM = " << nim;
+		cout << "\nNama = " << nama;
+
 	}
 
 };
+
+class Matakuliah {
+private:
+	int sks;
+	string kode;
+	string namaMk;
+
+public:
+	void inputMk() {
+		cout << "\nMasukan jumlah sks = ";
+	}
+};
+
